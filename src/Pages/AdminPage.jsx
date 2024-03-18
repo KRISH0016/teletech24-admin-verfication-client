1e4d6af7e80c643b226c353cfbc02ad3752d2343
krish0016 <pooannamalai09@gmail.com>
2024-03-18 22:32:13 +0530

    Overflow fix

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -8,7 +8,7 @@ import { api } from "../api/api";
 const AdminPage = () => {
   const [updateForm, setUpdateForm] = useState(false);
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="h-screen overflow-auto">
       <div className={`w-full h-[50px] flex items-center p-10 justify-between`}>
         <h1 className={`text-2xl font-bold`}>ADMIN PANEL</h1>
         <div className="w-[350px] flex items-center justify-between">
--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://teletech24-admin-verfication.onrender.com",
+  // baseURL: "https://teletech24-admin-verfication.onrender.com",
+  baseURL: "http://localhost:3005",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
